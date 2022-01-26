abdeb357866d3376686675a88db380a36c8a209b
Mehul Raj <71964538+MrM-7@users.noreply.github.com>
2022-01-26 13:54:34 +0530

    Create github.js

---
+++ js/github.js
@@ -0,0 +1,22 @@
+class GitHub{
+    constructor(){
+        this.client_id = '1c4837ec0b67f1693104';
+        this.client_secret = '6601865062904597988f16e51d9e4c7e5a6564f6';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.clientID}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.clientID}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
